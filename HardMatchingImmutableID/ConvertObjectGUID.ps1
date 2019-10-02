c892c594b7d79b20957aacd0394cb1afd3789633
Sabrina Kay <sabrinakay9988@gmail.com>
2019-10-02 18:32:45 +0800

    Create ConvertObjectGUID.ps1

---
+++ HardMatchingImmutableID/ConvertObjectGUID.ps1
@@ -0,0 +1,11 @@
+$list = Import-Csv .\list.csv -delimiter “,”
+
+foreach ($i in $list){
+$upn = $i.UserPrincipalName
+$guid = [GUID]$i.objectguid
+$bytearray = $guid.tobytearray()
+$immutableID = [system.convert]::ToBase64String($bytearray)
+
+write-host $immutableID
+
+}
