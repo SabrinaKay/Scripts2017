3ea49ebbdb7c3398934e69347e5054ec51aaff98
Sabrina Kay <sabrinakay9988@gmail.com>
2019-10-03 11:16:08 +0800

    Update SetNewImmutableID.ps1

--- HardMatchingImmutableID/SetNewImmutableID.ps1
+++ HardMatchingImmutableID/SetNewImmutableID.ps1
@@ -1,16 +1,15 @@
-#Import your final list 
+#Import your final list and set the new immutable id
+#Creator: Sabrina Kay
 
 Connect-MsolService
 
 $users = Import-Csv .\list.csv -delimiter “,”
 
 foreach ($i in $users)
-
 {
-$upn = $i.UserPrincipalName
-$guid = $i.objectguid
-$immutableid = $i.immutableid
+  $upn = $i.UserPrincipalName
+  $guid = $i.objectguid
+  $immutableid = $i.immutableid
 
-Set-MsolUser -UserPrincipalName $upn -ImmutableID $immutableid
-
+  Set-MsolUser -UserPrincipalName $upn -ImmutableID $immutableid
 }
